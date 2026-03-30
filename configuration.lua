c6d6af5c87fed9572efff7bf2be7460656606c9d
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2026-03-29 20:11:20 -0400

    Update configuration.lua

--- configuration.lua
+++ configuration.lua
@@ -756,7 +756,7 @@ end
 local ScreenGui = Instance.new("ScreenGui")
 ScreenGui.Name = "FlamesAdminGUI"
 ScreenGui.Parent = parent_gui
-ScreenGui.Enabled = true
+ScreenGui.Enabled = false
 ScreenGui.ResetOnSpawn = false
 
 local Frame = Instance.new("Frame")
